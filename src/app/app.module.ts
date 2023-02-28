65594c31fb9af90ae120bf680af8dcec5e7319f0
Kristopher Fana <kfana@africau.edu>
2023-02-28 14:11:37 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BackgroundImageComponent } from './background-image/background-image.component';
+import { LeftPanelComponent } from './background-image/left-panel/left-panel.component';
+import { RightPanelComponent } from './background-image/right-panel/right-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundImageComponent,
+    LeftPanelComponent,
+    RightPanelComponent
   ],
   imports: [
     BrowserModule
