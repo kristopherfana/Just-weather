56bc8c257d92238d7e0e15bea2e5a6206bab1c6d
Kristopher Fana <kfana@africau.edu>
2023-03-06 10:45:59 +0200

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BackgroundImageComponent } from './background-image/background-image.component';
-import { LeftPanelComponent } from './background-image/left-panel/left-panel.component';
-import { RightPanelComponent } from './background-image/right-panel/right-panel.component';
-import { TimeComponent } from './background-image/left-panel/time/time.component';
-import { MainInformationComponent } from './background-image/left-panel/main-information/main-information.component';
-import { MutedInformationComponent } from './background-image/left-panel/muted-information/muted-information.component';
+import { LeftPanelComponent } from './background-image/container/left-panel/left-panel.component';
+import { RightPanelComponent } from './background-image/container/right-panel/right-panel.component';
+import { TimeComponent } from './background-image/container/left-panel/time/time.component';
+import { MainInformationComponent } from './background-image/container/left-panel/main-information/main-information.component';
+import { MutedInformationComponent } from './background-image/container/left-panel/muted-information/muted-information.component';
+import { ContainerComponent } from './background-image/container/container.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { MutedInformationComponent } from './background-image/left-panel/muted-i
     RightPanelComponent,
     TimeComponent,
     MainInformationComponent,
-    MutedInformationComponent
+    MutedInformationComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
